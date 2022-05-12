fc22df06077c5dbb6006c0c0a4975c7ca7e02e50
jassy1995 <babatundejoseph85@gmail.com>
2022-05-12 17:58:14 +0100

    resolve error while deploying

--- src/components/nav-bar.jsx
+++ src/components/nav-bar.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/NavBar";
+import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Button from "react-bootstrap/Button";
