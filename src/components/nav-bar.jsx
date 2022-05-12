d035bc55a16c6ce8fe7c9f2a5a9b897382e03f62
jassy1995 <babatundejoseph85@gmail.com>
2022-05-12 21:06:58 +0100

    edited

--- src/components/nav-bar.jsx
+++ src/components/nav-bar.jsx
@@ -129,7 +129,7 @@ function NavBar({
           <Nav.Item>
             <strong>Categories</strong>
           </Nav.Item>
-          {categories.map((category) => (
+          {categories?.map((category) => (
             <Nav.Item key={category}>
               <LinkContainer
                 to={`/search?category=${category}`}
